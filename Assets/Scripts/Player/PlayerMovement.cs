8f33a7631f9040202ec591df81fbd1cca3c66c9a
Hunter Ovadia <hunter.ovadia@gmail.com>
2020-07-05 09:36:53 -0400

    Base Player Movement and Vital

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _movementSpeed = 5f;
+    [SerializeField] private float _rotationSpeed = 180f;
+
+    private CharacterController _controller;
+    private Vector3 _moveDirection = Vector3.zero;
+    private Vector3 _rotation;
+    private void Awake()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        _rotation = new Vector3(0, Input.GetAxisRaw("Horizontal") * _rotationSpeed * Time.deltaTime, 0f);
+        _moveDirection = new Vector3(0f, 0f, Input.GetAxisRaw("Vertical") * _movementSpeed * Time.deltaTime);
+        _moveDirection = transform.TransformDirection(_moveDirection);
+        _controller.Move(_moveDirection);
+        transform.Rotate(_rotation);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerVitals.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+
+public enum eVitalType
+{
+    HEALTH,
+}
+
+[Serializable]
+public class Vital
+{
+    public eVitalType Type;
+    public float Max;
+    public bool IsMaxOnAwake;
+
+    public float Current { get; private set; }
+    public delegate void OnUpdateDelegate(float newAmount, float newMax);
+    public OnUpdateDelegate OnUpdate;
+
+    public void Set(float newAmount)
+    {
+        Current = newAmount;
+        Current = Mathf.Clamp(Current, 0, Max);
+        OnUpdate?.Invoke(Current, Max);
+    }
+
+    public void Modify(float modAmount)
+    {
+        Current += modAmount;
+        Current = Mathf.Clamp(Current, 0, Max);
+        OnUpdate?.Invoke(Current, Max);
+    }
+
+}
+
+public class PlayerVitals : MonoBehaviour
+{
+    [SerializeField] private Vital[] Vitals;
+    private Dictionary<eVitalType, Vital> _vitalsDictionary = new Dictionary<eVitalType, Vital>();
+
+    private void Awake()
+    {
+        foreach(Vital v in Vitals)
+        {
+            if(v.IsMaxOnAwake)
+            {
+                v.Set(v.Max);
+            }
+            else
+            {
+                v.Set(0);
+            }
+
+            _vitalsDictionary.Add(v.Type, v);
+        }
+    }
+
+    public Vital GetVital(eVitalType type)
+    {
+        return _vitalsDictionary[type];
+    }
+}
